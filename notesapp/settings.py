2f22a7ca349eaba91b2ba1f83790b02f9c107fb1
Chimjiokem <chimjiokemanya@gmail.com>
2025-01-29 04:26:27 -0800

    Changed Debug to False, and set Allowed hosts

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6dmnm8tz4wwizi3rvdn7&m%$ol!dd(l6-bjg9k*!=j^#)9yzh0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
